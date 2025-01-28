84b48579542cc8a3fe4c61a98ecd82a50c6f4eb4
Prince Mendiratta <prince@getcreatr.xyz>
2025-01-29 00:16:00 +0530

    base

--- next.config.js
+++ next.config.js
@@ -5,6 +5,27 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: '*',
+				pathname: '**',
+			},
+		],
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	webpack: (config, { isServer }) => {
+		config.stats = "verbose";
+		return config;
+	},
+	output: "export"
+};
 
 export default config;
